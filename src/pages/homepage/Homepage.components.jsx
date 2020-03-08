feed9f26225a991dcfcf11d02d13b2704fae5bf9
Muhammad Ali <nerdjfpb@gmail.com>
2020-03-08 17:38:57 +0600

    adding css in javascript using styled-component

--- src/pages/homepage/Homepage.components.jsx
+++ src/pages/homepage/Homepage.components.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Directory from '../../components/directory/directory.component'
-import './homepage.styles.scss'
+import { HomepageContainer } from './Homepage.styles'
 
 const Homepage = () => {
   return (
-    <div className="homepage">
+    <HomepageContainer>
       <Directory />
-    </div>
+    </HomepageContainer>
   )
 }
 
